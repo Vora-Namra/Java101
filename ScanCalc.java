a70a1b87adb1abe32504b98b5c26ded12a6537da
Namra Vora <voranamra625@gmail.com>
2024-08-04 21:45:45 +0530

    add via commit

---
+++ ScanCalc.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class ScanCalc {
+public int operation(int num1,int num2 ,String operation) {
+        if(operation.equals("+")) {
+            return num1 + num2;
+        } else if(operation.equals("-")) {
+            return num1 - num2;
+        } else if(operation.equals("*")) {
+            return num1 * num2;
+        } else if(operation.equals("/")) {
+            return num1 / num2;
+        } else {
+            return 0;
+        }
+
+}
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter 1st Number: ");
+        int num1 = sc.nextInt();
+        System.out.println("Enter 2nd Number: ");
+        int num2 = sc.nextInt();
+        System.out.println("Enter operation as +, -, *, /: ");
+        String operation = sc.next();
+        ScanCalc obj = new ScanCalc();  
+        System.out.println("Result: " + obj.operation(num1, num2, operation));
+    }
+}
