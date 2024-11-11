f025506469ab99050f27143ee077d2c89ab0a483
Namra Vora <voranamra625@gmail.com>
2024-11-11 20:29:03 +0530

    remaining

---
+++ Interface/Engine.java
@@ -0,0 +1,10 @@
+package Interface;
+
+public interface Engine {
+
+    static final int price = 100000;
+    
+    void acc();
+    
+
+}
---
+++ Interface/Main.java
@@ -0,0 +1,7 @@
+package Interface;
+public class Main extends Car{
+    public static void main(String[] args) {
+        Engine car = new Car();
+        car.acc();
+    }
+}
